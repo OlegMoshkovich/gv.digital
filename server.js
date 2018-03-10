0370bce9529f6b5bd80184be7fdc1788b4eb909b
Oleg <oleg.mosh@gmail.com>
2018-03-09 21:19:59 -0500

    update

--- server.js
+++ server.js
@@ -5,12 +5,12 @@ app.get('/', (req, res) => {
   res.sendFile(`${__dirname}/index.html`)
 })
 
-// app.listen(3000, () => {
-//   console.log('Example app listening on port 3000!')
-// })
+app.listen(3000, () => {
+  console.log('Example app listening on port 3000!')
+})
 
-app.listen(process.env.PORT, process.env.IP, function() {
-  console.log('I am listening');
-});
+// app.listen(process.env.PORT, process.env.IP, function() {
+//   console.log('I am listening');
+// });
 
 app.use(express.static(`${__dirname}/`))
